1e7b02bff63062f6af29311ffe6417b59d5180dd
kenkuo86 <kenkuo860129@gmail.com>
2018-09-12 00:49:19 +0800

    2

--- googleform.js
+++ googleform.js
@@ -1,10 +1,41 @@
 $(document).ready(function(){
 	let a1 = document.getElementById('A1');
-	if (a1.onfocus) {
-		if (a1.innerHTML === ""){
-			alert("此欄位必填!")
-		}
-	}
+	let a2 = document.getElementById('A2');
 	
+	let a4 = document.getElementById('A4');
+	a1.addEventListener("focusout",function(){
+		if (a1.value === "") {
+			$(this).parent().addClass("alert");
+			$(this).addClass("alert");
+			$(this).next().html("此欄位必填，請輸入內容");
+		} else {
+			$(this).parent().removeClass("alert");
+			$(this).removeClass("alert");
+			$(this).next().html("");
+		}
+	});
+	a2.addEventListener("focusout",function(){
+		if (a2.value === "") {
+			$(this).parent().addClass("alert");
+			$(this).addClass("alert");
+			$(this).next().html("此欄位必填，請輸入內容");
+		} else {
+			$(this).parent().removeClass("alert");
+			$(this).removeClass("alert");
+			$(this).next().html("");
+		}
+	});
+	
+	a4.addEventListener("focusout",function(){
+		if (a4.value === "") {
+			$(this).parent().addClass("alert");
+			$(this).addClass("alert");
+			$(this).next().html("此欄位必填，請輸入內容");
+		} else {
+			$(this).parent().removeClass("alert");
+			$(this).removeClass("alert");
+			$(this).next().html("");
+		}
+	});
 
 });
