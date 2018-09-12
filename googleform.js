37a5191c0683bdaae59ba2e7ba14ceb41a39097a
kenkuo86 <kenkuo860129@gmail.com>
2018-09-12 20:13:06 +0800

    4

--- googleform.js
+++ googleform.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	let a1 = document.getElementById('A1');
 	let a2 = document.getElementById('A2');
 	let a4 = document.getElementById('A4');
+	let a5 = document.getElementById('A5');
 	a1.addEventListener("focusout",function(){
 		if (a1.value === "") {
 			$(this).parent().addClass("alert");
@@ -42,7 +43,14 @@ $(document).ready(function(){
 				$("#Q3").next().next().next().next().html("不可以兩者都選，請取消其中一個。");
 				$("#Q3").parent().addClass('alert');
 			} else {
-				console.log(a1.value,'\n',a2.value,'\n',a4.value);
+				$("#Q3").next().next().next().next().html("");
+				$("#Q3").parent().removeClass('alert');
+				if ($("input#pro")[0].checked) {
+					console.log(a1.value,'\n',a2.value,'\n','工程師培養班','\n',a4.value,'\n',a5.value);
+				} else {
+					console.log(a1.value,'\n',a2.value,'\n','業餘班','\n',a4.value,'\n',a5.value);
+				}
+				alert("表單內容已經顯示在console中了!");
 			}
 		} else {
 			$("#Q3").next().next().next().next().html("此欄位必選，請選擇其中一項");
@@ -50,16 +58,4 @@ $(document).ready(function(){
 
 		}
 	});
-
-	// console.log(a1.value);
-	// $("input#pro")[0].click(function(){
-	// 	if ($("input#pro")[0].checked) {
-	// 		$("input#pro")[0].checked = false;
-	// 	} else {
-	// 		$("input#pro")[0].checked = true;
-	// 	}
-	// });
-
-
-
 });
